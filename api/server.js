4e0e36627c0b1b25f5fbe4c4e797b087b03860ce
ivanmoroni92 <ivan.mor92@gmail.com>
2021-07-15 17:17:32 -0300

    back

--- api/server.js
+++ api/server.js
@@ -2,24 +2,28 @@ const express = require("express");
 const app = express();
 const PORT = 3001;
 const db = require("./db/index");
+const volleyball = require("volleyball");
 const routes = require("./routes");
-const {Plan} = require("./models");
+const { Plan } = require("./models");
 
 // respond with "hello world" when a GET request is made to the homepage
 app.get("/", function (req, res) {
   res.send("hello world");
 });
 
-app.use("/api" , routes)
+app.use(express.json());
+app.use(volleyball);
 
-// const eventTest = new Plan({
-//   name: "eventTest3",
-// });
+app.use("/api", routes);
 
-// eventTest.save((err) => {
-//   if (err) return console.error(err);
-//   console.log("guardado!");
-// });
+app.use((error, req, res, next) => {
+  console.log(error);
+  if ((error.name = "castError")) {
+    res.status(400).end();
+  } else {
+    res.status(500).end();
+  }
+});
 
 db.on("error", console.error.bind(console, "connection error:"));
 db.once("open", function () {
