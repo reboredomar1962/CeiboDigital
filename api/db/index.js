a0146db5631fe348716ec167ee378772ed2117f5
ivanmoroni92 <ivan.mor92@gmail.com>
2021-07-19 11:48:51 -0300

    ivan

--- api/db/index.js
+++ api/db/index.js
@@ -1,8 +1,10 @@
 const mongoose = require("mongoose");
-mongoose.connect("mongodb://localhost:27017/ceiboDigital", {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
+mongoose
+  .set("useCreateIndex", true)
+  .connect("mongodb://localhost:27017/ceiboDigital", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
 
 const db = mongoose.connection;
 module.exports = db;
