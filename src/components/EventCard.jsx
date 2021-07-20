219279601cc43200790dac79303dd532edecabf3
agreloe <agreloerica@gmail.com>
2021-07-20 08:03:30 -0300

    FlatLine corregido creo

--- src/components/EventCard.jsx
+++ src/components/EventCard.jsx
@@ -1,70 +1,69 @@
 import * as React from 'react';
-import { StyleSheet, Text, View, ScrollView, SafeAreaView } from "react-native";
-import axios from 'axios';
-import { showPlans } from '../state/plan';
+import { StyleSheet, Text, View, ScrollView, SafeAreaView, FlatList } from "react-native";
+import plansReducer, { showPlans } from '../state/plan';
 import { useSelector, useDispatch } from 'react-redux'
-
 import { Card, Title, Paragraph } from 'react-native-paper';
-
-
-const promotedEvents = [
-  {
-    title: 'Personal Fest',
-    paragraph: 'Entradas disponibles',
-    image: 'https://images.unsplash.com/photo-1514525253161-7a46d19cd819?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1934&q=80',
-    id: 1
-  },
-  {
-      title: 'Feria Masticar',
-      paragraph: 'Conseguí tus entradas',
-      image: 'https://images.unsplash.com/photo-1571946479349-e652b8a44817?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=2250&q=80',
-      id: 2
-  },
-  {
-    title: 'Bar Terraza',
-    paragraph: 'After Office',
-    image: 'https://images.unsplash.com/photo-1517457373958-b7bdd4587205?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2250&q=80',
-    id: 3
-}
-]
-
 
 
 const EventCard = () => {
 
-  const { plans } = useSelector(store => store.plan);
+  const plans = useSelector(store => store.plan);
   const dispatch = useDispatch();
-  const planes = []
-  const pedido = () =>{
-    return axios.get('http://localhost:3001/api/plan')
-    .then(res => planes.push(res.data))
-    .catch(error => console.log('ACA ESTA EL ERROR DE NUEVO -----> ',error))
-  }
 
   React.useEffect(()=>{
-    /* dispatch(showPlans())
-    console.log('ACA ESTA PLANS---->',plans) */
-    pedido()
-    
+    dispatch(showPlans())
   })
 
-  return (
+  const Item = ({id, img, name, description}) => (
+    <View style={styles.cardContainer} >
+        
+            <Card style={styles.cardStyle} key={id}>
+                <Card.Cover source={{uri: img[0]}} />
+                <Card.Content style={{marginTop:5}}>
+                    <Title style={styles.titleTxt}>{name}</Title>
+                    <Paragraph style={styles.paragTxt} >{description}</Paragraph>
+                </Card.Content>
+            </Card>
+        
+      </View>
+  );
+
+  const renderItem = ({item}) => (
+    <Item id={item.id} img={item.img} name={item.name} description={item.description} />
+  );
+
+  /* return (
+    
     <ScrollView horizontal={true} showsHorizontalScrollIndicator={false} >
       <View style={styles.cardContainer} >
         {
           promotedEvents.map(event => (
             <Card style={styles.cardStyle} key={event.id}>
-                <Card.Cover source={{uri: event.image}} />
+                <Card.Cover source={{uri: event.img[0]}} />
                 <Card.Content style={{marginTop:5}}>
-                    <Title style={styles.titleTxt}>{event.title}</Title>
-                    <Paragraph style={styles.paragTxt} >{event.paragraph}</Paragraph>
+                    <Title style={styles.titleTxt}>{event.name}</Title>
+                    <Paragraph style={styles.paragTxt} >{event.description}</Paragraph>
                 </Card.Content>
             </Card>
           ))
         }
       </View>
-    </ScrollView>
-  )
+    </ScrollView> 
+
+  ) */
+
+return(
+  
+    <SafeAreaView>
+    <FlatList horizontal={true}
+    data={plans}
+    renderItem={renderItem}
+    keyExtractor={item => item.id}
+    
+    />
+    </SafeAreaView>
+)
+
       };
 
 export default EventCard;
