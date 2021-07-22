13c6dd7b7e50f1cd2ddcbc854360556287b43975
ajromeroch <ale.romero07@gmail.com>
2021-07-22 17:39:16 -0400

    modificacion desde github

--- src/state/categories.js
+++ src/state/categories.js
@@ -7,15 +7,18 @@ import {
 import { Platform } from "react-native";
 
 const initialState = {
-    categories: [],
-}
+  categories: [],
+};
 
-export const showCategories = createAsyncThunk('SHOW_CATEGORIES', ()=>{
-    const os = Platform.OS === 'android' ? '10.0.2.2' : 'localhost'
-    return axios.get(`http://${os}:3001/api/category`)
-    .then(res => res.data)
-    .catch(error => console.log('ACA ESTA EL ERROR DE CATEGORIES -----> ',error))  
-})
+export const showCategories = createAsyncThunk("SHOW_CATEGORIES", () => {
+  const os = Platform.OS === "android" ? "10.0.2.2" : "localhost";
+  return axios
+    .get(`http://${os}:3001/api/category`)
+    .then((res) => res.data)
+    .catch((error) =>
+      console.log("ACA ESTA EL ERROR DE CATEGORIES -----> ", error)
+    );
+});
 
 export const showCategories = createAsyncThunk("SHOW_CATEGORIES", () => {
   const os = Platform.OS === "android" ? "10.0.2.2" : "localhost";
@@ -25,16 +28,8 @@ export const showCategories = createAsyncThunk("SHOW_CATEGORIES", () => {
     .catch((error) => console.log("ACA ESTA EL ERROR -----> ", error));
 });
 
-<<<<<<< HEAD
-const categoriesReducer = createReducer(initialState, {
+const categoriesReducer = createReducer([], {
   [showCategories.fulfilled]: (state, action) => action.payload,
 });
 
 export default categoriesReducer;
-=======
-const categoriesReducer = createReducer([], {
-    [showCategories.fulfilled] : (state, action) => action.payload,
-})
-  
-export default categoriesReducer;
->>>>>>> ffc66c64dc1acd4225931e741cec8ef45e09c439
