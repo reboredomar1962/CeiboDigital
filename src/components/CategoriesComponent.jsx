0abe3e0ca698ded78b298eee849e21ccecb0c802
ajromeroch <ale.romero07@gmail.com>
2021-07-22 17:34:01 -0400

    categoria

--- src/components/CategoriesComponent.jsx
+++ src/components/CategoriesComponent.jsx
@@ -5,37 +5,49 @@ import { useDispatch, useSelector } from "react-redux";
 import { showCategories } from "../state/categories";
 
 // const categories = [
-//     {name: 'Negocios', image: 'https://images.unsplash.com/photo-1505373877841-8d25f7d46678?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2000&q=80', id: 1},
-//     {name: 'Gastronomía', image: 'https://images.unsplash.com/photo-1414235077428-338989a2e8c0?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=2250&q=80', id: 2},
-//     {name: 'Fiestas', image: 'https://images.unsplash.com/photo-1514525253161-7a46d19cd819?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1867&q=80', id: 3},
-//     {name: 'Deportes', image: 'https://images.unsplash.com/photo-1552674605-db6ffd4facb5?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=2100&q=80', id: 4},
-// ]
+//   {
+//     type: "Negocios",
+//     img: "<https://images.unsplash.com/photo-1505373877841-8d25f7d46678?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2000&q=80",
+//     id: 1,
+//   },
+//   {
+//     type: "Gastronomía",
+//     img: "https://images.unsplash.com/photo-1414235077428-338989a2e8c0?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=2250&q=80",
+//     id: 2,
+//   },
+//   {
+//     type: "Fiestas",
+//     img: "https://images.unsplash.com/photo-1514525253161-7a46d19cd819?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1867&q=80",
+//     id: 3,
+//   },
+//   {
+//     type: "Deportes",
+//     img: "https://images.unsplash.com/photo-1552674605-db6ffd4facb5?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=2100&q=80",
+//     id: 4,
+//   },
+// ];
 
 const CategoriesComponent = () => {
   const dispatch = useDispatch();
 
   const categories = useSelector((store) => {
-    store.categories;
-    console.log("linea 19 ->", store.categories);
+    return store.categories;
   });
 
   React.useEffect(() => {
     dispatch(showCategories());
-
-    console.log("ACA ESTA LAS CATEGORIES->", categories);
   }, []);
 
   return (
     <View style={styles.cardCont}>
-      {categories &&
-        categories.map((category) => (
-          <Card style={styles.cardStyle} key={category.id}>
-            <Card.Cover source={{ uri: category.img }} />
-            <Card.Content style={{ marginTop: 5 }}>
-              <Title style={styles.titleTxt}>{category.type}</Title>
-            </Card.Content>
-          </Card>
-        ))}
+      {categories.map((category) => (
+        <Card style={styles.cardStyle} key={category.id}>
+          <Card.Cover source={{ uri: category.img }} />
+          <Card.Content style={{ marginTop: 5 }}>
+            <Title style={styles.titleTxt}>{category.type}</Title>
+          </Card.Content>
+        </Card>
+      ))}
     </View>
   );
 };
