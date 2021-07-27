fa137c2ea3ef904171d58d507802a517a5c7ba37
ajromeroch <ale.romero07@gmail.com>
2021-07-27 14:22:39 -0400

    cambio search screen

--- src/screens/SearchScreen.jsx
+++ src/screens/SearchScreen.jsx
@@ -1,17 +1,25 @@
 import * as React from "react";
-import { StyleSheet, Text, View, ScrollView, SafeAreaView, Modal, Pressable } from "react-native";
+import {
+  StyleSheet,
+  Text,
+  View,
+  ScrollView,
+  SafeAreaView,
+  Modal,
+  Pressable,
+} from "react-native";
 //-------------Redux Import------------------------------
 import { useSelector, useDispatch } from "react-redux";
 import { showCategories } from "../state/categories";
 //-------------Libraries Import--------------------------
 import { Card, Title, Paragraph } from "react-native-paper";
-import { AntDesign } from '@expo/vector-icons'; 
+import { AntDesign } from "@expo/vector-icons";
 
 //------------Components Import-----------------------------
 import Search from "../components/Search";
 import CategoriesComponent from "../components/CategoriesComponent";
 import SearchedPlans from "../components/SearchedPlans";
-
+import { TextInput } from "react-native-gesture-handler";
 
 const SearchScreen = ({ navigation }) => {
   const { searchedPlans } = useSelector((store) => {
@@ -24,9 +32,11 @@ const SearchScreen = ({ navigation }) => {
   return (
     <SafeAreaView style={styles.container}>
       <ScrollView>
-          
         <View>
-          <Search navigation={navigation} setModal={()=>setModalVisible(true)}/>
+          <Search
+            navigation={navigation}
+            setModal={() => setModalVisible(true)}
+          />
 
           {searchedPlans.length !== 0 ? (
             <View>
@@ -39,40 +49,78 @@ const SearchScreen = ({ navigation }) => {
             </View>
           )}
 
+          <Modal
+            style={{ marginTop: 50 }}
+            animationType="fade"
+            transparent={true}
+            visible={modalVisible}
+            onRequestClose={() => {
+              setModalVisible(!modalVisible);
+            }}
+            presentationStyle="overFullScreen"
+          >
+            <View
+              style={{
+                flex: 1,
 
-
-      <Modal
-        animationType="fade"
-        transparent={true}
-        visible={modalVisible}
-        onRequestClose={() => {
-          setModalVisible(!modalVisible);
-        }}
-        presentationStyle='overFullScreen'
-      >
-        <View style={{width:'50%', backgroundColor:'red',  flex:1, justifyContent:'center', alignItems:'center', flexDirection:'column' }}>
-        <Pressable
-              
-              onPress={() => setModalVisible(!modalVisible)}
+                justifyContent: "center",
+                alignItems: "flex-start",
+                flexDirection: "column",
+              }}
             >
-              <AntDesign name="close" size={24} color="black" />
-            </Pressable>
+              <View
+                style={{
+                  width: "75%",
+                  height: "80%",
+                  backgroundColor: "blue",
+                  justifyContent: "center",
+                  alignItems: "center",
+                  flexDirection: "column",
+                  borderRadius: 10,
+                }}
+              >
+                <View>
+                  {/* Maneja la X para salir*/}
+                  <Pressable onPress={() => setModalVisible(!modalVisible)}>
+                    <AntDesign name="close" size={24} color="black" />
+                  </Pressable>
+                </View>
+                <View>
+                  <Text>Fecha</Text>
+                </View>
+                <View>
+                  <Text>Precio</Text>
+                  <TextInput placeholder="Mínimo" onEndEditing={() => {}} />
+                  <TextInput placeholder="Máximo" onEndEditing={() => {}} />
+                </View>
 
-        <Text>Holis?</Text>
-        </View>
-
-
-      </Modal>
-
+                <View>
+                  <Text>Recomendación</Text>
+                </View>
+                <View>
+                  <Text>Privado?</Text>
+                </View>
+                <View>
+                  <Text>Gratis?</Text>
+                </View>
+              </View>
+            </View>
+          </Modal>
         </View>
       </ScrollView>
-
-
-
-
     </SafeAreaView>
   );
 };
+
+// planDate,
+// planDateBefore,
+// planDateAfter,
+// address,
+// priceMin,
+// priceMax,
+// recommendation,
+// private,
+// free,
 
 export default SearchScreen;
 
