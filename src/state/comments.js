b4a099f8dc6be7376417855ea5ef6920aa2eaebe
martina reboredo <reboredomar1962@gmail.com>
2021-07-30 13:42:57 -0300

    removePlan

--- src/state/comments.js
+++ src/state/comments.js
@@ -11,7 +11,7 @@ export const createComment = createAsyncThunk("CREATE_COMMENT", (commentObj) => 
   console.log('LLEGAMOS AKKIIIII', commentObj)
   return AsyncStorage.getItem("token")
     .then((token) => {
-      return axios.post(`http://10.0.2.2:3001/api/plan/${planId}/comments`, commentObj, {
+      return axios.post(`http://192.168.200.22:3001/api/plan/${planId}/comments`, commentObj, {
         headers: { Authorization: `Bearer ${JSON.parse(token)}` },
       });
     })
@@ -22,7 +22,7 @@ export const createComment = createAsyncThunk("CREATE_COMMENT", (commentObj) => 
 export const showComments = createAsyncThunk("SHOW_COMMENTS", (planId) => {
   return AsyncStorage.getItem("token")
     .then((token) => {
-      return axios.get(`http://10.0.2.2:3001/api/plan/${planId}/comments`, {
+      return axios.get(`http://192.168.200.22:3001/api/plan/${planId}/comments`, {
         headers: { Authorization: `Bearer ${JSON.parse(token)}` },
       });
     })
