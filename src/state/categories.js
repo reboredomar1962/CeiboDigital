c3eccd2f304846f8be133ed24954c059ede7888f
martina reboredo <reboredomar1962@gmail.com>
2021-08-04 15:20:17 -0300

    marti

--- src/state/categories.js
+++ src/state/categories.js
@@ -13,10 +13,10 @@ const initialState = {
 
 export const showCategories = createAsyncThunk("SHOW_CATEGORIES", () => {
   const os = Platform.OS === "android" ? "10.0.2.2" : "localhost";
-  const ip = "192.168.0.3";
+  const ip = "192.168.200.22";
   return axios
 
-    .get(`http://${os}:3001/api/category`)
+    .get(`http://192.168.200.22:3001/api/category`)
 
     .then((res) => res.data)
     .catch((error) =>
@@ -27,7 +27,7 @@ export const showCategories = createAsyncThunk("SHOW_CATEGORIES", () => {
 export const addCategory = createAsyncThunk("ADD_CATEGORY", () => {
   const os = Platform.OS === "android" ? "10.0.2.2" : "localhost";
   return axios
-  .post(`http://${os}:3001/api/category`)
+  .post(`http://192.168.200.22:3001/api/category`)
   .then((res) => res.data)
   .catch((error) =>
     console.log("ACA ESTA EL ERROR DE ADD_CATEGORIES -----> ", error)
